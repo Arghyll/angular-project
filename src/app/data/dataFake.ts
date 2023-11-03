e4928e9d15983184d015d7dde5dd553b94413104
Diego <carvalho.84@gmail.com>
2023-11-03 11:39:42 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,36 @@
+import { style } from "@angular/animations"
+
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "aaa",
+    "title": "Só tome cuidado com o Dark Urge",
+    "description": "Você não se lembra de nada além de um caminho pavimentado com sangue",
+    "photoCover": "https://blog.playstation.com/tachyon/2023/07/57e7d11ff5648496f28d278ab870f5688e8efc1a-scaled.jpg?resize=1088%2C612&crop_strategy=smart&zoom=1.5"
+  },
+
+  {
+    "id": "2",
+    "photo": "aaa",
+    "title": "Shadowheart ",
+    "description": "Uma devotada clériga de Shar, deusa das trevas e da perda, Shadowheart concordou em ter suas memórias apagadas como parte de uma missão sagrada. Agora sua única sobrevivente, ela deve entregar uma relíquia poderosa de volta aos seus parentes para conquistar o amor de Shar e ter suas memórias restauradas, mas o tempo todo, ela é atormentada por uma magia estranha e dolorosa que ela luta para controlar.",
+    "photoCover": "https://topmovies.com.br/wp-content/uploads/2023/09/1694094424_524_Shadowheart-e-realmente-mais-util-se-ela-estiver-morta-em.jpg"
+  },
+
+  {
+    "id": "3",
+    "photo": "aaa",
+    "title": "karlach",
+    "description": "Recém-escapada do Inferno, Karlach está finalmente livre do arquidiabo Zariel, mas não da máquina infernal que Zariel plantou em seu peito. Com seu primeiro gostinho de liberdade em dez anos, Karlach está ansiosa para encontrar uma solução para o motor que está queimando cada vez mais antes de queimá-la completamente. Mas ainda mais importante em sua mente? Explorar, encontrar viajantes que pensam como você, apaixonar-se (ou desejar)... e vingar-se do homem que a vendeu para Zariel anos atrás.",
+    "photoCover": "https://oyster.ignimgs.com/mediawiki/apis.ign.com/baldurs-gate-3/b/b7/KARLACH.png?width=814"
+  },
+
+  {
+    "id": "4",
+    "photo": "aaa",
+    "title": "Gale",
+    "description": "Gale de Águas Profundas foi um arquimago prodigioso e seus talentos lhe renderam a atenção de Mystra, a deusa da magia. Ao lado dela, ele aprendeu tudo o que os mortais poderiam realizar com a Trama, conquistando com o tempo seu lugar como seu amante e seu Escolhido. E ainda assim, ele não estava satisfeito.",
+    "photoCover": "https://www.pcinvasion.com/wp-content/uploads/2023/09/best-gale-build-in-baldurs-gate-3.jpg?resize=800%2C400"
+  },
+
+]
